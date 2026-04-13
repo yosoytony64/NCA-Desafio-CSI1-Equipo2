a31e789956a896c6c33f08c05329a41d7a82c73a
Alberto <albertobarcelo22@gmail.com>
2026-04-13 12:32:04 +0200

    ADD CONEXION JAVA --> SQL

--- src/caballos365/Caballos365.java
+++ src/caballos365/Caballos365.java
@@ -20,27 +20,9 @@ public class Caballos365 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
+        pantalla obj_pantalla = new pantalla();
+        obj_pantalla.setVisible(true);
+                }    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
