bf870c2e427071bd687d3eefa71bf8d9ba2c655a
Alberto <albertobarcelo22@gmail.com>
2026-04-13 12:32:37 +0200

    ADD CONEXION JAVA --> SQL

---
+++ src/caballos365/Conexion.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caballos365;
+import java.sql.*;
+/**
+ *
+ * @author alber
+ */
+public class Conexion {
+   static String url="jdbc:mysql://localhost:3306/casino";
+   static String user="root";
+   static String pass="Barsi.22";
+    
+    public static Connection conectar() {
+    Connection con = null;
+    try {
+        // Forzamos el driver por si acaso
+        
+        con = DriverManager.getConnection(url, user, pass);
+        System.out.println("Conexión exitosa");
+    } catch (SQLException e) {
+        e.printStackTrace();
+        System.out.println("Error interno: " + e.getMessage());
+    }
+    return con; // Si falla, devuelve null
+}
+}
